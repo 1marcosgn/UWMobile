913e06ed7c6f37fe9d39c361ec2d1e792af94048
Marcos Garcia <1marcosgn@gmail.com>
2015-11-29 11:27:15 -0800

    Initial Commit

---
+++ UWMobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UWMobile
+//
+//  Created by Marcos Garcia on 11/29/15.
+//  Copyright © 2015 Marcos Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UWMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UWMobile
+//
+//  Created by Marcos Garcia on 11/29/15.
+//  Copyright © 2015 Marcos Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
